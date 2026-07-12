6a8c7470eda2fc4dc94721d4b7f1838bf7963491
Indu Kosaraju <indukosaraju@Indus-MacBook-Air.local>
2026-07-12 12:13:05 +0530

    Added wrapper classes programs

---
+++ wrapperClasses/Q4.java
@@ -0,0 +1,40 @@
+class Employee implements Cloneable {
+
+    int id;
+    String name;
+    double salary;
+
+    Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class Q4 {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        Employee e1 = new Employee(101, "Indu", 50000);
+
+        Employee e2 = (Employee) e1.clone();
+
+        e1.name = "Kiran";
+        e1.salary = 60000;
+
+        System.out.println("Original Employee:");
+        e1.display();
+
+        System.out.println("\nCloned Employee:");
+        e2.display();
+    }
+}
