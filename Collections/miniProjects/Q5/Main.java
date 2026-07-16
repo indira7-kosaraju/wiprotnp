fe69709336d2880c8c53c3a42870182fc8735876
Indira Kosaraju <indukosaraju7@gmail.com>
2026-07-16 14:31:22 +0530

    Added miniprojects

---
+++ Collections/miniProjects/Q5/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of Box");
+        int n = sc.nextInt();
+
+        Set<Box> set = new TreeSet<>(Comparator.comparingDouble(Box::getVolume));
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.println("Enter the Box " + i + " details");
+
+            System.out.println("Enter Length");
+            double length = sc.nextDouble();
+
+            System.out.println("Enter Width");
+            double width = sc.nextDouble();
+
+            System.out.println("Enter Height");
+            double height = sc.nextDouble();
+
+            set.add(new Box(length, width, height));
+        }
+
+        System.out.println("Unique Boxes in the Set are");
+
+        for (Box b : set) {
+            System.out.println(b);
+        }
+
+        sc.close();
+    }
+}
